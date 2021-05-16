353d0b43d190b6d2e7b94f0514131b6cb192c083
Attila Varga <root@PP.localdomain>
2021-05-16 16:15:03 +0100

    rename

--- deploy_cicd.sh
+++ deploy_cicd.sh
@@ -1,4 +1,4 @@
 sam deploy \
   --template-file cfn-tests/GitSyncJobRepository.yaml \
   --capabilities CAPABILITY_IAM --stack-name GitSyncJobRepository \
-  --parameter-overrides $(cat ./cfn-tests/GitSyncJobRepository.params)
+  --parameter-overrides $(cat ./cicd/GitSyncJobRepository.params)
